32143dc2d28d7978c6b6e7922b2ac8f375d3d50a
Valentin Commenge <txreplay@gmail.com>
2018-01-22 23:21:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <header>
+      <img class="logo" src="./assets/images/logo.png" alt="logo">
+    </header>
+    <MainTable/>
+  </div>
+</template>
+
+<script>
+import MainTable from './components/MainTable'
+
+export default {
+  name: 'App',
+  components: {
+    MainTable
+  },
+}
+</script>
+
+<style lang="scss">
+@import './assets/styles/custom-bootstrap.scss';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+html, body {
+  height: 100%;
+  margin: 0;
+  background: #333;
+  background: -webkit-linear-gradient(left top, #30cfd0, #330867);
+  background:    -moz-linear-gradient(bottom right, #30cfd0, #330867);
+  background:         linear-gradient(to bottom right, #30cfd0, #330867);
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+
+  header {
+    margin-bottom: 20px;
+    
+    .logo {
+      max-width: 200px;
+      padding: 15px 0 0 20px;
+    }
+  }
+}
+</style>
---
+++ src/components/MainTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-fluid">
+    <div class="maintable table-responsive">
+      <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col" colspan="2" rowspan="2"></th>
+            <th>Ledger</th>
+            <th>Kraken</th>
+            <th scope="col" rowspan="2">Add new</th>
+          </tr>
+          <tr>
+            <th>€ Ledger</th>
+            <th>€ Kraken</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">BTC</th>
+            <td>Total BTC</td>
+            <td>Ledger BTC</td>
+            <td>Kraken BTC</td>
+          </tr>
+          <tr>
+            <th scope="row">ETH</th>
+            <td>Total ETH</td>
+            <td>Ledger ETH</td>
+            <td>Kraken ETH</td>
+          </tr>
+          <tr>
+            <th scope="row">Add new</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainTable',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
